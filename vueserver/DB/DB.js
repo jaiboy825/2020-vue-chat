a823d5a242e0d5d30b2f74ca7ad7a1e58182085f
jaiboy825 <jaiboy@naver.com>
2020-11-04 20:56:50 +0900

    11월 04일 클래스룸 영상 시청 완료

--- vueserver/DB/DB.js
+++ vueserver/DB/DB.js
@@ -1,7 +1,14 @@
 const mysql = require('mysql2');
 const {c} = require('./credential');
 
-//jsp connection pool
+//jsp connection pool (스포닝 풀)
 // Driver.getConnection
 const pool = mysql.createPool(c);
-//js 비동기 언어, ECMA2015(Promise)
+
+const promisePool = pool.promise();
+
+module.exports = {
+    pool: promisePool
+};
+
+
--- vueserver/server.js
+++ vueserver/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const {pool} = require('./DB/DB');
 
 const app = new express();
 const server = http.createServer(app);
@@ -12,8 +13,22 @@ app.set('view engine', 'ejs');
 
 app.use( express.static( path.join(__dirname, 'public') ) );
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res)=>{
     res.render('main');
+});
+
+app.get('/test', async (req, res)=>{
+    let r = await pool.query("SELECT * FROM dummy");
+    res.json(r[0]);
+});
+
+app.post('/data', async (req, res)=>{
+    //req.body.title
+    //let r = await pool.query("select * from dummy");
+    //res.json(r[0])
 });
 
 server.listen(54000, ()=>{
